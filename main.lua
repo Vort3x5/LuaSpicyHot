aa429e9db07caae8286fae292fe3da0311703e4c
Vort3x5 <Faking.Games857@gmail.com>
2025-02-17 18:55:03 +0100

    scale_units reduced

--- main.lua
+++ main.lua
@@ -2,7 +2,6 @@
 
 local fltk = require("fltk4lua")
 
--- Resistor
 local resistors = {}
 
 local function AddResistor(resistance, conduction, current, voltage)
@@ -15,29 +14,12 @@ local function AddResistor(resistance, conduction, current, voltage)
 	table.insert(resistors, resistor)
 end
 
--- Scale units
-local voltage_scale_units = {
-	V = 1,
-	mV = 0.001,
-	kV = 1000,
-}
-
-local current_scale_units = {
-	A = 1,
-	mA = 0.001,
-	kA = 1000,
-}
-
-local resistance_scale_units = {
-	OM = 1,
-	mOM = 0.001,
-	kOM = 1000,
-}
-
-local conduction_scale_units = {
-	S = 1,
-	mS = 0.001,
-	kS = 1000,
+local scale_units = {
+	n = 0,000001,
+	m = 0.001,
+	X = 1,
+	k = 1000,
+	M = 1000000,
 }
 
 local window = fltk.Window(400, 300, arg[0])
