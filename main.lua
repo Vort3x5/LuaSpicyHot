ef115c28531ad29880ca0dedaa38e24de30e1421
Vort3x5 <Faking.Games857@gmail.com>
2025-02-19 19:31:07 +0100

    Drawing

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
- local love = require("love")
+local love = require("love")
 
 local resistors = {}
 
@@ -15,13 +15,13 @@ end
 local scale_units = {
 	n = 0,000001,
 	m = 0.001,
-	X = 1,
 	k = 1000,
 	M = 1000000,
 }
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
+GRID_SIZE = 10
 
 function love.load()
 	love.window.setTitle("LuaSpicyHot")
@@ -35,20 +35,28 @@ function love.update(dt)
 end
 
 function love.draw()
-	DrawGrid()
+	DrawElement(640, 360, Cursor)
 end
 
-function DrawGrid()
-    -- Set the grid color to black
-    love.graphics.setColor(0, 0, 0)
+function DrawElement(x, y, Element)
+	love.graphics.setColor(0, 0, 0)
+	love.graphics.setLineWidth(3)
+
+	Element(x, y)
+end
+
+function DrawCursor(x, y)
+	love.graphics.setColor(0, 0, 1)
 	love.graphics.setLineWidth(2)
+	love.graphics.line(x, y, x + GRID_SIZE, y)
+	love.graphics.line(x - GRID_SIZE, y, x, y)
+	love.graphics.line(x, y, x, y + GRID_SIZE)
+	love.graphics.line(x, y, x, y - GRID_SIZE)
+end
 
-	grid_size = 10
-    for x = 0, WINDOW_WIDTH, grid_size do
-        love.graphics.line(x, 0, x, WINDOW_HEIGHT)
-    end 
-
-    for y = 0, WINDOW_HEIGHT, grid_size do
-        love.graphics.line(0, y, WINDOW_WIDTH, y)
-    end
+function Resistor(x, y)
+	love.graphics.line(x - GRID_SIZE, y, x + GRID_SIZE, y)
+	love.graphics.line(x - GRID_SIZE, y, x - GRID_SIZE, y - 3*GRID_SIZE)
+	love.graphics.line(x + GRID_SIZE, y, x + GRID_SIZE, y - 3*GRID_SIZE)
+	love.graphics.line(x - GRID_SIZE, y - 3*GRID_SIZE, x + GRID_SIZE, y - 3*GRID_SIZE)
 end
